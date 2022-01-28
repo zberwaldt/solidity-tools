8cd02d480567e39562b45993876e1e2fae0a27ae
Zach Berwaldt <zberwaldt@tutamail.com>
2022-01-27 21:02:11 -0500

    first commit

---
+++ solidity-tools.py
@@ -0,0 +1,55 @@
+import sublime
+import sublime_plugin
+import os
+import glob
+import subprocess
+
+C = {}
+DEFAULT_CONTENT = """//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract Contract {
+
+}"""
+
+class CreateSolidityContractCommand(sublime_plugin.WindowCommand):
+  def run(self):
+    hardhat_config = 'hardhat.config.ts'
+    open_folders = self.window.folders()
+    for path in open_folders:
+      contents = os.listdir(path)
+      if hardhat_config in contents:
+        C['PROJECT_ROOT'] = path
+
+      # find contracts folder.
+      if 'contracts' in contents:
+        C['CONTRACTS_DIR'] = os.path.join(C['PROJECT_ROOT'], 'contracts')
+      else:
+        print('contracts directory doesn\'t exist')
+        contracts_path = os.path.join(C['PROJECT_ROOT'], 'contracts')
+        os.makedirs(contracts_path)
+
+      # create a new solidity contract.
+      # new_file_at {"dirs": ["/home/zberwaldt/workspace/_bankless-dao/roots/contracts"]}
+      self.window.run_command('side_bar_new_file', {
+        "paths": [C['CONTRACTS_DIR']]
+      })
+
+class CreateSolidityTestCommand(sublime_plugin.WindowCommand):
+  def run(self):
+    print('create solidity test...')
+
+class CreateSolidityScriptCommand(sublime_plugin.WindowCommand):
+  def run(self):
+    print('create solidity script...')
+
+class InitializeHardhatProjectCommand(sublime_plugin.WindowCommand):
+  def run(self):
+    print('initializing hardhat project...')
+    folder = os.getcwd()
+    print(folder)
+    # subprocess.Popen(['touch', 'file.txt'])
+
+
